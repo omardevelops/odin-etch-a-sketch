cbac56d116bf6d0e78de4d89f129091a81014b3d
Omar Ahmad <superomar121@gmail.com>
2021-07-06 14:24:17 +0400

    added hover event listener, changes square color

--- etchasketch.js
+++ etchasketch.js
@@ -1,5 +1,5 @@
 const grid_container = document.querySelector('#grid_container');
-console.log(grid_container);
+// console.log(grid_container);
 
 function createGrid(rows, columns) {
     for (let i = 0; i < rows; i++) {
@@ -9,6 +9,17 @@ function createGrid(rows, columns) {
         }
     }
     grid_container.style.gridTemplateColumns = `repeat(${columns}, 1fr)`;
+    grid_container.style.gridTemplateRows = `repeat(${rows}, 1fr)`;
 }
 
-console.log(createGrid(16,16));
+function addEventListenersToDivs() {
+    const divs = Array.from(grid_container.getElementsByTagName('div'));
+    divs.forEach(function (div) {
+        div.addEventListener('mouseenter', function() {
+            div.style.background = 'yellow';
+        });
+    });
+}
+
+createGrid(16,16);
+addEventListenersToDivs();
